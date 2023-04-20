80d2b1fd0fa16007adaffce3227cbb0382df0596
Lucas Santos <lucassnts963@gmail.com>
2023-04-20 17:28:47 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+function Home() {
+  return <>
+    <h1>Você é um amigo sensacional, saiba que o dia que precisar pode contar comigo!</h1>
+    <p>Escolhi você para enviar essa simples mensagem, nesse formato diferente. Além de mim, saiba que Deus é o seu maior amigo, você pode contar com ele 24hrs por dia 7 dias por semana. Deus te abençoe sempre</p>
+    <p>Além de mim, saiba que Deus é o seu maior amigo, você pode contar com ele 24hrs por dia 7 dias por semana. Deus te abençoe sempre!</p>
+  </>
+}
+
+export default Home
